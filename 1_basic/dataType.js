0f1e70b8d7731489da50b9e8225856cba19c6e0d
AnujSingh200 <161486434+AnujSingh200@users.noreply.github.com>
2025-01-28 12:08:25 +0000

    config

---
+++ 1_basic/dataType.js
@@ -0,0 +1,29 @@
+// Primitive data type are a set of basic data type in javaScript.
+// there are 7 types of primitive data types
+
+// String
+// number
+// bigInt
+// null
+// boolean
+// undefined
+// symbol
+
+let name ="Anuj"
+let age=23
+let value=142554548512965295689563295632956956653259695645522655555545455141455114788
+let city=null
+let isYou=true
+let iAm;
+
+console.log(typeof city)
+//console.log(typeof name)
+
+
+// object is derived from primitive data type
+
+const item={
+    name : "led Bulb",
+    price: 150
+}
+console.log(item);
---
+++ 1_basic/variables.js
@@ -0,0 +1,13 @@
+const accountId=12335
+let accountEmail="singh@google.com"
+var accountPassword="12234"
+accountCity="Prayagraj"
+
+console.table([accountId, accountEmail,accountPassword,accountCity]);
+
+// accountId=123
+accountEmail="anuj@abc.com"
+accountPassword="212121"
+accountCity="kanpur"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
